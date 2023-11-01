f8e82c74254fd57de103ed27f6904a0f9aea1ffb
Udit bhadauria <UDITBHADAURIA.23041058@gehu.ac.in>
2023-11-01 12:14:30 +0530

    subtraction added

--- q-9.c
+++ q-9.c
@@ -26,29 +26,43 @@ int rem(int a,int b){
 
 void main(){
 
-
-    printf("    1. for addition\n 2. for subtraction\n 3. for multiplication\n 4. for division\n 5. for remainder\n 6. for exit\n");
-
-    int x;
-    printf("ENTER ANY DIGIT:-");
-    scanf("%d",&x);
+    int x=0;
+    
 
     do{
+        printf(" 1. for addition\n 2. for subtraction\n 3. for multiplication\n 4. for division\n 5. for remainder\n 6. for exit\n");
+
+        printf("ENTER YOUR CHOICE:= ");
+        scanf("%d",&x);
+
         if (x==1){
             int a;
-            printf("ENTER ANY DIGIT:-");
+            printf("ENTER FIRST DIGIT:= ");
             scanf("%d",&a);
             
             int b;
-            printf("ENTER ANY DIGIT:-");
+            printf("ENTER SENCOND DIGIT:= ");
             scanf("%d",&b);
             
             int c = addi(a,b);
-            printf("THE SUM OF THESE NUMBERS ARE %d",c);
-        }while (x!=5);
+            printf("THE SUM OF THESE NUMBERS ARE %d\n\n",c);
+        }
+
+        else if(x==2){
+            int a;
+            printf("ENTER FIRST DIGIT:= ");
+            scanf("%d",&a);
+            
+            int b;
+            printf("ENTER SENCOND DIGIT:= ");
+            scanf("%d",&b);
+            
+            int c = subtra(a,b);
+            printf("THE Sub OF THESE NUMBERS ARE %d\n\n",c);
+        }
+    }while(x!=6);
         
 
 
-    }
-   
+
 }
